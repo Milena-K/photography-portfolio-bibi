a5deb1411cc2b43e25090e613d11f5987c03ef4c
186085 <milena.kukolj@students.finki.ukim.mk>
2023-03-24 22:48:59 +0100

    Create basic home page.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import { Overpass_Mono } from 'next/font/google'
+
+const overpass = Overpass_Mono({
+  weight: '300',
+  subsets: ['latin'],
+})
+
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={overpass.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
